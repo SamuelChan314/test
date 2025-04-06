b6ea5267a34052aae3ebd337e0a9e8c0c66b039f
SamuelChan314 <69959819+SamuelChan314@users.noreply.github.com>
2025-04-06 02:43:58 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
+  basePath: '/test', // Replace with your repository name
+  assetPrefix: '/test/', // Replace with your repository name
 };
 
 module.exports = nextConfig;
